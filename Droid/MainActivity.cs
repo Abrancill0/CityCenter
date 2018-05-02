d103ec5d58f4a573c9cc3c197932a0b3af91984a
Abrancill0 <38171762+Abrancill0@users.noreply.github.com>
2018-05-02 11:37:35 -0500

    Using DIalogs

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -12,6 +12,7 @@ using CarouselView.FormsPlugin.Android;
 using FFImageLoading.Forms.Droid;
 using Naxam.Controls.Platform.Droid;
 using Xamarin;
+using Acr.UserDialogs;
 //using FFImageLoading.Forms.Droid;
 
 namespace City_Center.Droid
